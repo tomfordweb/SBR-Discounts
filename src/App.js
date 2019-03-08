db3c7d0de5bce242d535d4a619e74ce12f265e36
Tom Ford <tomfordweb@gmail.com>
2019-03-07 20:15:21 -0500

    dotenv added, update to react hooks, basic logic working

--- src/App.js
+++ src/App.js
@@ -4,55 +4,17 @@ import DiscountForm from './DiscountForm';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import "react-tabs/style/react-tabs.css";
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      uidDiscount: '00000000-0000-0000-0000000000000000',
-      vchDiscountCode: 'deleteme',
-      decDiscount: '5.00',
-      booPercentage: '0',
-      vchDiscountDescription: 'dev purposes',
-      uidEvent: '',
-      uidEventType: 'E45E2796-A916-6BD8-97619EBFDF81E3D6',
-      intClubMemberType: [
-        1024, 1, 2, 256, 512, 128, 64, 32, 16, 8, 4
-      ],
-      booRestrictFirstEvent: '0',
-      sntQuantityAvailable: ' 1',
-      sntMaxRedemptions: '1',
-      mnyThresholdMin: '0.00',
-      mnyDiscountMax: ' 5.00',
-      dteStart: ' 03/06/1990',
-      dteEnd: ' 03/07/1990',
-      quantity: 51,
-    };
 
 
-  }
 
-  // format { key : value }
-  // merge data with currentState
-  changedInput = (data) => {
-    this.setState((prevState) => ({
-      prevState,
-      ...data
-    }));
-  }
 
-  render() {
-    const {
-      changedInput
-    } = this;
 
-    return (
-      <div className="App">
-
+export default function App () {
+  return (
+    <div className="App">
         <header>
           <h1>Bulk Discount Creator</h1>
-          <p>Enter the amount of codes you wish to create. Then click "Submit"</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, debitis voluptatum dicta pariatur deserunt blanditiis impedit nesciunt minima quia recusandae voluptatibus saepe nam numquam, nemo autem repellat excepturi repellendus. Nisi.</p>
         </header>
         <main>
           <Tabs>
@@ -62,15 +24,13 @@ class App extends Component {
               <Tab>Response</Tab>
               <Tab>Generated Codes</Tab>
             </TabList>
-
             <TabPanel>
-              <DiscountForm data={this.state} onChange={changedInput} />
+              <DiscountForm />
               <footer>
-                <input type="submit" value="Submit Form"/>
               </footer>
             </TabPanel>
             <TabPanel>
-              <pre>{ JSON.stringify(this.state, null, 2)}</pre>
+              <pre></pre>
             </TabPanel>
             <TabPanel>
               <h2>Response Fields</h2>
@@ -85,8 +45,5 @@ class App extends Component {
           <p>Made By <a href="http://tomfordweb.com" target="_blank">Tom Ford</a> - <a href="mailto:tomfordweb@gmail.com">Support</a></p>
         </footer>
       </div>
-    );
-  }
+  )
 }
-
-export default App;
--- src/DiscountForm.jsx
+++ src/DiscountForm.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import useAsyncEndpoint from './API';
 
+const organizationId = process.env.REACT_APP_MSR_ORGANIZATION_ID;
+const username = process.env.REACT_APP_MSR_USERNAME;
+const password = process.env.REACT_APP_MSR_PASSWORD;
+const route = 'https://api.motorsportreg.com/rest/discounts.json';
 
-export const Input = ({
-  name, value, label, type, onChange, helpText
-}) => (
+export const Input = ({ name, value, label, type, onChange, helpText }) => (
   <div>
     { (label) &&
       <label>{label}</label>
@@ -12,7 +17,7 @@ export const Input = ({
     <input
       id={name}
       type={type}
-      onChange={(e) => onChange({[name] : e.target.value})}
+      onChange={onChange}
       name={name}
       value={value}
     />
@@ -23,50 +28,104 @@ export const Input = ({
   </div>
 )
 
+function createDiscount() {
+  console.log(organizationId, username, password);
+  return useAsyncEndpoint(data => ({
+    method: "GET",
+    headers: {
+      'X-Organization-Id' : organizationId
+    },
+    auth: {
+      username,
+      password
+    },
+    responseType: 'json',
+    // data: data,
+    url: route
+  }));
+}
 
-const DiscountForm = ({ data : {
-    quantity, decDiscount, vchDiscountDescription,
-  },
-  onChange }) => (
-  <article className="discountForm">
-    <section>
-      <Input
-        name="quantity"
-        value={quantity}
-        type="number"
-        onChange={onChange}
-        label="Quantity"
-        helpText="The amount of discount codes to create"
-      />
-      <Input
-        name="decDiscount"
-        value={decDiscount}
-        type="number"
-        onChange={onChange}
-        label="Discount Value"
-        helpText="The value of the discount code"
-      />
-      <Input
-        name="vchDiscountDescription"
-        value={vchDiscountDescription}
-        type="text"
-        onChange={onChange}
-        label="Description"
-        helpText="A description for the discount code."
-      />
-      <div>
-        <label>Expiration:</label>
-        <div className="wrap">
-        <select>
-          <option value="">The next 3 months</option>
-          <option value="">The next year (365 Days From Now)</option>
-          <option value="">Forever</option>
-        </select>
-        <span className="formHelpText">How long the codes will last</span>
+
+function DiscountForm({}) {
+
+  // make a date object that is one year from now
+  const oneYrFromNow = new Date().setFullYear(new Date().getFullYear() + 1);
+
+  // first time using hooks!
+  const [quantity, setQuantity] = useState(5);
+  const [decDiscount, setDecDiscount] = useState(5);
+  const [vchDiscountDescription, setVchDiscountDescription ] = useState('Dev Purposes');
+  const [dteStart, setDteStart] = useState(new Date());
+  const [dteEnd, setDteEnd] = useState(new Date(oneYrFromNow));
+  const [discount, postNewDiscount] = createDiscount();
+
+  function newDiscount() {
+    postNewDiscount({
+      uidDiscount: '00000000-0000-0000-0000000000000000',
+      vchDiscountCode: 'deleteme',
+      decDiscount: decDiscount,
+      booPercentage: '0',
+      vchDiscountDescription: vchDiscountDescription,
+      uidEvent: '',
+      uidEventType: 'E45E2796-A916-6BD8-97619EBFDF81E3D6',
+      intClubMemberType: [
+        1024, 1, 2, 256, 512, 128, 64, 32, 16, 8, 4
+      ],
+      booRestrictFirstEvent: '0',
+      sntQuantityAvailable: ' 1',
+      sntMaxRedemptions: '1',
+      mnyThresholdMin: '0.00',
+      mnyDiscountMax: ' 5.00',
+      dteStart: dteStart,
+      dteEnd: dteEnd,
+    })
+  }
+  return (
+    <div>
+        <Input
+          name="quantity"
+          value={quantity}
+          type="number"
+          onChange={e => setQuantity(e.target.value)}
+          label="Quantity"
+          helpText="The amount of discount codes to create"
+        />
+        <Input
+          name="decDiscount"
+          value={decDiscount}
+          type="number"
+          onChange={e => setDecDiscount(e.target.value)}
+          label="Discount Value"
+          helpText="The value of the discount code"
+        />
+        <Input
+          name="vchDiscountDescription"
+          value={vchDiscountDescription}
+          type="text"
+          onChange={e => setVchDiscountDescription(e.target.value)}
+          label="Description"
+          helpText="A description for the discount code."
+        />
+        <div>
+        <label>Valid From</label>
+        <DatePicker
+          selected={dteStart}
+          onChange={date => setDteStart(date)}
+        />
         </div>
-      </div>
-    </section>
-</article>
-)
+        <div>
+        <label>Valid To</label>
+        <DatePicker
+          selected={dteEnd}
+          onChange={date => setDteEnd(date)}
+        />
+        </div>
+
+        <div>
+        <button onClick={newDiscount}>Submit Form</button>
+        </div>
+    </div>
+  )
+}
 
 export default DiscountForm;
